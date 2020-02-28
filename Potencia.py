1df61e2352b45049b86e3ae3000be0cda085b581
ANDERAAM <48502025+ANDERAAM@users.noreply.github.com>
2020-02-27 19:28:00 -0500

    Add files via upload

---
+++ Potencia.py
@@ -0,0 +1,23 @@
+import string
+import sys
+import os
+
+Lista=[];
+
+l = "L={#,c,d}";separador=l[3]+l[5]+l[7]
+Lista.append(l[3]);Lista.append(l[5]);Lista.append(l[7]);
+
+ 
+ n1=len(a);n2=len(b)
+ for i in range(len(a)):
+     for p in range(len(b)):
+        c.append(a[i]+b[p])
+ print("esta es la lista resultante : ",c) 
+
+
+
+print(Lista);
+
+
+num=6;
+
---
+++ p2.py
@@ -0,0 +1,53 @@
+import string
+import sys
+import os
+
+Lista=[];
+p=sys.argv[2];
+l = sys.argv[1];separador=l[3]+l[5]+l[7]
+Lista.append(l[3]);Lista.append(l[5]);Lista.append(l[7]);
+
+ 
+ n1=len(a);n2=len(b)
+ for i in range(len(a)):
+     for p in range(len(b)):
+        c.append(a[i]+b[p])
+ print("esta es la lista resultante : ",c) 
+
+
+
+print(Lista);
+
+
+num=6;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
